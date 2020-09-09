5944fbf5d4de1b8455c66950d3358e82b04d4aa2
周小娟 <zhouxiaojuan@nnuo.com>
2020-09-09 14:04:08 +0800

    poseMessage

--- js-interview/js-webpack/app/Layout.js
+++ js-interview/js-webpack/app/Layout.js
@@ -2,52 +2,29 @@ import React from 'react';
 import { HashRouter as Router, Link, Route, Switch, useHistory, Redirect } from 'react-router-dom';
 
 import { Button } from 'antd';
-// import logo from './logo.svg';
-// import './App.css';
-
-const Login = () => {
-  const history = useHistory();
-  const login = () => {
-    history.push('/product/a');
-  };
-  return (
-    <div>
-      <h2>我是登录页</h2>
-      <Button type="primary" onClick={login}>
-        登录
-      </Button>
-    </div>
-  );
-};
-
-const ProductA = () => <div>aaaaaaaaa</div>;
-const ProductB = () => <div>bbbbb</div>;
-const ProductC = () => <div>cccc</div>;
-
-const Product = () => (
-  <div>
-    <h2>我是工作台</h2>
-    <Link to="/product/a">a</Link>
-    <Link to="/product/b">b</Link>
-    <Link to="/product/c">c</Link>
-    <Switch>
-      <Route path="/product/a" component={ProductA} />
-      <Route path="/product/b" component={ProductB} />
-      <Route path="/product/c" component={ProductC} />
-    </Switch>
-  </div>
-);
 function App() {
+  const sendPost = () => {
+    // 获取id为otherPage的iframe窗口对象
+
+    var iframeWin = document.getElementById('otherPage').contentWindow;
+    // 向该窗口发送消息
+    iframeWin.postMessage({ king: document.getElementById('message').value }, '*');
+  };
+
+  // 监听跨域请求的返回
+
+  window.addEventListener(
+    'message',
+    function (event) {
+      console.log(event, event.data, '-----dasdasdasd-------------');
+    },
+    false,
+  );
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          {/* 重定向 如你启动时候地址是没有login的，就匹配不到路由，这个可以帮你重定向到login */}
-          <Redirect exact path="/" to="/login" />
-          <Route path="/login" exact component={Login} />
-          <Route path="/product" component={Product} />
-        </Switch>
-      </Router>
+      <textarea id="message"></textarea>
+      <input type="button" value="发送" onClick={sendPost} />
+      <iframe src="http://172.30.5.42:8000" id="otherPage" width={500} height={500}></iframe>
     </div>
   );
 }
